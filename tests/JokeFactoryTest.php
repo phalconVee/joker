063cc83f4cd7075d61349f116023af298108c1bf
Henry Ugochukwu <phalcon.vee@gmail.com>
2020-11-09 10:09:11 +0100

    Apply fixes from StyleCI (#2)

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -6,7 +6,6 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
-use PhalconVee\Joker\helper\GuzzleRequestHelper;
 use PhalconVee\Joker\JokeFactory;
 use PHPUnit\Framework\TestCase;
 
@@ -23,7 +22,7 @@ class JokeFactoryTest extends TestCase
 
         $result = $jokes->getRandomJoke();
 
-       $this->assertSame('If tapped, a Chuck Norris roundhouse kick could power the country of Australia for 44 minutes.', $result);
+        $this->assertSame('If tapped, a Chuck Norris roundhouse kick could power the country of Australia for 44 minutes.', $result);
     }
 
     private function get_api_mock($status, $body = null)
